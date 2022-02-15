c85e161e4587102fd4ea3466720f8a1aa84cb3ce
jfaulk3 <81659039+AmateurSlacker@users.noreply.github.com>
2022-02-15 03:53:41 -0600

    Stylized NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,48 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
 
 export default function Nav() {
   return (
-    <header className="bg-cyan-500">
-      <div>
-        <nav>
-          <NavLink to="/" exact="true">
+    <header className="bg-red-500">
+      <div className="container mx-auto flex justify-between">
+        <nav className="flex">
+          <NavLink
+            to="/"
+            exact="true"
+            className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest"
+          >
             Home
           </NavLink>
-          <NavLink to="about">About Me</NavLink>
-          <NavLink to="project">Projects</NavLink>
+          <NavLink
+            to="about"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+          >
+            About Me
+          </NavLink>
+          <NavLink
+            to="project"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+          >
+            Projects
+          </NavLink>
         </nav>
+        <div className="inline-flex py-3 px-3 my-6">
+          <SocialIcon
+            url="https://github.com/jfaulk3"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://www.linkedin.com/in/faulknerjimmie3/"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+        </div>
       </div>
     </header>
   );

eeafea6a57d159055833fb9e474e0f9755ca23ab
jfaulk3 <faulknerjimmie@gmail.com>
2022-02-15 15:14:31 -0600

    Projects page filled/stylized

---
+++ portfolioupdated/schemas/project.js
@@ -0,0 +1,51 @@
+export default {
+  name: "project",
+  title: "Project",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+    },
+    {
+      name: "date",
+      type: "datetime",
+    },
+    {
+      name: "github_link",
+      type: "url",
+    },
+    {
+      name: "description",
+      type: "text",
+    },
+    {
+      name: "projectType",
+      title: "Project type",
+      type: "string",
+      options: {
+        list: [
+          { value: "personal", title: "Personal" },
+          { value: "client", title: "Client" },
+          { value: "school", title: "School" },
+        ],
+      },
+    },
+    {
+      name: "live_link",
+      type: "url",
+    },
+    {
+      name: "tags",
+      type: "array",
+      of: [
+        {
+          type: "string",
+        },
+      ],
+      options: {
+        layout: "tags",
+      },
+    },
+  ],
+};
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,81 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import sanityClient from "../client";
 
 export default function Project() {
-  return <h1>Projects Page</h1>;
+  const [projectData, setProjectData] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "project"]{
+      title,
+      github_link,
+      description,
+      projectType,
+      live_link,
+      tags
+    }`
+      )
+      .then((data) => setProjectData(data))
+      .catch(console.error);
+  }, []);
+  return (
+    <main className="bg-gray-200 min-h-screen p-12">
+      <section className="container mx-auto">
+        <h1 className="text-5xl flex justify-center cursive">My Projects</h1>
+        <h2 className="text-lg text-gray-600 flex justify-center mb-12">
+          Welcome to my projects page!
+        </h2>
+        <section className="grid grid-cols-2 gap-8">
+          {projectData &&
+            projectData.map((project, index) => (
+              <article
+                key={project.title + index}
+                className="relative rounded-lg shadow-xl bg-white p-16"
+              >
+                <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-700">
+                  <a
+                    href={project.live_link}
+                    alt={project.title}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {project.title}
+                  </a>
+                </h3>
+                <div className="text-gray-500 text-xs space-x-5">
+                  <a
+                    href={project.github_link}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className="text-red-500 font-bold hover:underline hover:text-red-400"
+                  >
+                    <span>
+                      <strong className="font-bold">Github Repository</strong>:{" "}
+                      {project.github_link}
+                    </span>
+                  </a>
+                  <span>
+                    <strong className="font-bold">Type</strong>:{" "}
+                    {project.projectType}
+                  </span>
+                  <p className="my-6 text-lg text-gray-700 leading-relaxed">
+                    {project.description}
+                  </p>
+                  <a
+                    href={project.live_link}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className="text-red-500 font-bold hover:underline hover:text-red-400"
+                  >
+                    👉🏾 View The Project Live!
+                    <span role="img" aria-label="right pointer"></span>
+                  </a>
+                </div>
+              </article>
+            ))}
+        </section>
+      </section>
+    </main>
+  );
 }

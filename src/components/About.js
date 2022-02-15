dcb58e77328f1581c9ad571e44fbe5ec5692ef91
jfaulk3 <faulknerjimmie@gmail.com>
2022-02-15 17:07:38 -0600

    Added info/stylized about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import sanityClient from "../client.js";
+import image from "../author-splash-image.jpg";
+import imageUrlBuilder from "@sanity/image-url";
+import BlockContent from "@sanity/block-content-to-react";
 
 export default function About() {
-  return <h1>About Page</h1>;
+  const [author, setAuthor] = useState(null);
+
+  const builder = imageUrlBuilder(sanityClient);
+  function urlFor(source) {
+    return builder.image(source);
+  }
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "author"]{
+      name,
+      bio,
+      "authorImage": image.asset->url
+    }`
+      )
+      .then((data) => setAuthor(data[0]))
+      .catch(console.error);
+  }, []);
+
+  if (!author) return <div>Loading...</div>;
+  return (
+    <main className="relative">
+      <img
+        src={image}
+        alt="a computer filled with code"
+        className="absolute w-full"
+      />
+      <div className="p-10 lg:pt-48 container mx-auto relative">
+        <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
+          <img
+            src={urlFor(author.authorImage).url()}
+            alt={author.name}
+            className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"
+          />
+          <div className="text-lg flex flex-col justify-center">
+            <h1 className="cursive text-6xl text-green-300 mb-4">
+              Hey! I'm <span className="text-green-100">{author.name}</span>
+            </h1>
+            <div className="prose lg:prose-xl text-white">
+              <BlockContent
+                blocks={author.bio}
+                projectId="fe67loqs"
+                dataset="production"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+    </main>
+  );
 }
